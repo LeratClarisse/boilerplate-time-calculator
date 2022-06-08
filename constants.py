129c8d540eae81ff8dc2bca32abb124f82627908
Lerat Clarisse <clarisselerat@hotmail.fr>
2022-06-08 12:45:21 +0000

    simple addition + AM/PM

---
+++ constants.py
@@ -0,0 +1,4 @@
+DAYS_OF_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+NEXT_DAY = "(next day)"
+DAYS_LATER = "days later"
--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,26 @@
-def add_time(start, duration):
+import constants
 
+def add_time(start, duration, first_day = ""):
+  #Init values
+  hours = int(start.split(" ")[0].split(":")[0])
+  minutes = int(start.split(" ")[0].split(":")[1])
+  am_pm = start.split(" ")[1]
 
+  #Add duration
+  hours += int(duration.split(":")[0])
+  minutes += int(duration.split(":")[1])
 
+  #Check if AM/PM change
+  if hours > 12:
+    if am_pm == "AM":
+      am_pm = "PM"
+    else:
+      am_pm = "AM"
+    hours -= 12
 
+  #Check if day change
+  
+  #Format result
+  new_time = str(hours) + ":" + str(minutes).zfill(2) + " " + am_pm
 
-    return new_time
+  return new_time
